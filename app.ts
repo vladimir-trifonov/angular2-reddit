b64a103d7f25f3b2877c779f92764ab00743e25e
Vladimir Trifonov <vladimir.trifonov@clevertech.biz>
2015-08-30 16:04:17 +0300

    rendering multiple components

--- app.ts
+++ app.ts
@@ -8,22 +8,73 @@ import {
 } from "angular2/angular2";
 
 @Component({
-	selector: 'hello-world'
+	selector: 'reddit-article'
 })
 @View({
-	directives: [NgFor],
 	template: `
-	<ul>
-		<li *ng-for="#name of names">Hello {{name}}</li>
-	</ul>
-`
+	<article>
+		<div class="votes">{{votes}}</div>
+		<div class="main">
+			<h2>
+				<a href="{{link}}">{{title}}</a>
+			</h2>
+			<ul>
+				<li><a href (click)='voteUp()'>upvote</a></li>
+				<li><a href (click)='voteDown()'>upvote</a></li>
+			</ul>
+		</div>
+	</article>
+	`
 })
-class HelloWorld {
-		names: Array<string>;
+class RedditArticle {
+		votes: number;
+		title: string;
+		link: string;
 
 		constructor() {
-			this.names = ['Felipe', 'Ari', 'Carlos'];
+				this.votes = 10;
+				this.title = 'Angular 2';
+				this.link = 'http://angular.io';
+		}
+
+		voteUp() {
+				this.votes += 1;
+				return false;
+		}
+
+		voteDown() {
+				this.votes -= 1;
+				return false;
 		}
 }
 
-bootstrap(HelloWorld);
+
+@Component({
+	selector: 'reddit'
+})
+@View({
+	directives: [RedditArticle],
+	template: `
+	<section class="new-link">
+		<div class="control-group">
+			<div><label for="title">Title:</label></div>
+			<div><input name="title" #newtitle></div>
+		</div>
+		<div class="control-group">
+			<div><label for="link">Link:</label></div>
+			<div><input name="link" #newlink></div>
+		</div>
+
+		<button (click)="addArticle(newtitle, newlink)">Submit Link</button>
+	</section>
+
+	<reddit-article></reddit-article>
+`
+})
+class RedditApp {
+		addArticle(title, link) {
+				console.log("Adding article with title", title.value, "and link", link.value);
+		}
+}
+
+bootstrap(RedditApp);
