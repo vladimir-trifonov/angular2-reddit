e1040ec97f33d5ce8dc18f01cdf9a0018b57c832
Vladimir Trifonov <vladimir.trifonov@clevertech.biz>
2015-08-30 16:34:38 +0300

    rendered multiple components

--- app.ts
+++ app.ts
@@ -5,55 +5,63 @@ import {
 	NgFor,
 	View,
 	bootstrap
-} from "angular2/angular2";
+}
+from "angular2/angular2";
 
-@Component({
-	selector: 'reddit-article'
-})
-@View({
+class Article {
+	title: string;
+	link: string;
+	votes: number;
+
+	constructor(title, link) {
+		this.title = title;
+		this.link = link;
+		this.votes = 0;
+	}
+
+	voteUp() {
+		this.votes += 1;
+		return false;
+	}
+
+	voteDown() {
+		this.votes -= 1;
+		return false;
+	}
+}
+
+@
+Component({
+	selector: 'reddit-article',
+	properties: ['article']
+})@ View({
 	template: `
 	<article>
-		<div class="votes">{{votes}}</div>
+		<div class="votes">{{ article.votes }}</div>
 		<div class="main">
 			<h2>
-				<a href="{{link}}">{{title}}</a>
+				<a href="{{ article.link }}">{{ article.title }}</a>
 			</h2>
 			<ul>
-				<li><a href (click)='voteUp()'>upvote</a></li>
-				<li><a href (click)='voteDown()'>upvote</a></li>
+				<li><a href (click)='article.voteUp()'>upvote</a></li>
+				<li><a href (click)='article.voteDown()'>downvote</a></li>
 			</ul>
 		</div>
 	</article>
 	`
 })
 class RedditArticle {
-		votes: number;
-		title: string;
-		link: string;
-
-		constructor() {
-				this.votes = 10;
-				this.title = 'Angular 2';
-				this.link = 'http://angular.io';
-		}
-
-		voteUp() {
-				this.votes += 1;
-				return false;
-		}
-
-		voteDown() {
-				this.votes -= 1;
-				return false;
-		}
+	votes: number;
+	title: string;
+	link: string;
 }
 
 
-@Component({
+@
+Component({
 	selector: 'reddit'
-})
-@View({
-	directives: [RedditArticle],
+})@ View({
+	directives: [RedditArticle, NgFor],
 	template: `
 	<section class="new-link">
 		<div class="control-group">
@@ -68,13 +76,22 @@ class RedditArticle {
 		<button (click)="addArticle(newtitle, newlink)">Submit Link</button>
 	</section>
 
-	<reddit-article></reddit-article>
+	<reddit-article *ng-for="#article of articles" [article]="article"></reddit-article>
 `
 })
 class RedditApp {
-		addArticle(title, link) {
-				console.log("Adding article with title", title.value, "and link", link.value);
-		}
+	articles: Array < article > ;
+
+	constructor() {
+			this.articles = [
+					new Article('Angular 2', 'http://angular.io'),
+					new Article('Fullstack', 'http://angular.io')
+			];
+	}
+
+	addArticle(title, link) {
+		console.log("Adding article with title", title.value, "and link", link.value);
+	}
 }
 
 bootstrap(RedditApp);
